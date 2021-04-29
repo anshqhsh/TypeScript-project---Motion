379fb980f7f5b32ae17f7e3bcd17387599371bcc
LJH's MacBookPro16 <60730765+anshqhsh@users.noreply.github.com>
2021-04-29 21:48:53 +0900

    make Component for Encapsulate

---
+++ src/components/component.ts
@@ -0,0 +1,19 @@
+export interface Component {
+    attachTo(parent: HTMLElement, position?: InsertPosition): void
+}
+/**
+ * Encapsulate the HTML elment creation
+ */
+export class BaseComponent<T extends HTMLElement> implements Component{
+    protected readonly element: T; //자식클래스에서만 접근가능하고, 한번 만들어지면 재사용 불가 
+    //Basecomponent 사용시 어떤 component를 받을껀지 받아서 사용 
+    constructor(htmlString: string) {//string타입의 element를 받아서 만들어짐 
+        const template = document.createElement('template');
+        template.innerHTML = htmlString;
+        this.element = template.content.firstElementChild! as T;
+    }
+
+    attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin' ){
+        parent.insertAdjacentElement(position, this.element) //자식요소 어딘가에 추가하는 API
+    }
+}
--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,14 +1,12 @@
-export class ImageComponent {
-    private element  : HTMLElement
-    //생성자에서 title과 url을 전달 받아서 동적으로 만듦 template테그 이용
-    constructor(title: string, url: string){
-        const template = document.createElement('template');
-        template.innerHTML = `<section class="image">
-        <div class="image__holder"><img class="image__thumbnail"></div>
-        <p class="image__title"></p>
-    </section>`;//InnerHTML을 이용하여 스트링 타입으로 이용할 수 있음 - template안에 있는 첫번째 자식
-        this.element = template.content.firstElementChild! as HTMLElement;
+import { BaseComponent } from '../../component.js';
 
+export class ImageComponent extends BaseComponent<HTMLElement>{
+    constructor(title: string, url: string){
+        super(`<section class="image">
+                <div class="image__holder"><img class="image__thumbnail"></div>
+                <p class="image__title"></p>
+                </section>`);
+        
         //innerHTML에 데이터를 전달하지 않고 따로 필요한 데이터만 업데이트를 시켜준다.
         const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement; //Class 명이 image__thumnail인것을 받아옴
         imageElement.src = url;
@@ -17,7 +15,4 @@ export class ImageComponent {
         const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement; //casting
         titleElement.textContent = title;
     }
-    attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin' ){
-        parent.insertAdjacentElement(position, this.element) //자식요소 어딘가에 추가하는 API
-}
 }
