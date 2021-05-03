fa5a555d5a9ebe4bbb98dfc0f3ae2999abed8196
LJH's MacBookPro16 <60730765+anshqhsh@users.noreply.github.com>
2021-05-03 14:20:22 +0900

    리펙토링

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { Composable, PageComponent } from './components/page/page.js';
+import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
 
 class App{
     private readonly page: Component & Composable
     //추가할 최상위의 루트 요소를 받아옴
     constructor(appRoot: HTMLElement){
-        this.page = new PageComponent();//새로운페이지 컴포넌트를 만들어
+        this.page = new PageComponent(PageItemComponent);//새로운페이지 컴포넌트를 만들어
         this.page.attachTo(appRoot);//appRoot에 붙여줘
         
         const image = new ImageComponent('Image Title', 'https://picsum.photos/200/300');
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -6,7 +6,16 @@ export interface Composable {
 }
 type OnCloseListener = () => void; // 닫힌상태만 알려주는 콜백함수
 
-class PageItemComponent extends BaseComponent<HTMLElement> implements Composable{
+//규격사항 : 섹션을 감싸는 컨테이너  
+interface SectionContainer extends Component, Composable {
+    setOnCloseListener(listener: OnCloseListener): void
+}
+
+type SectionContainerConstructor = {
+    new(): SectionContainer//아무것도 전달 받지 않는 생성자 생성자를 받는 어떤 클래스도 받는다 생성자가 호출 받으면 
+}
+
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer{
     private closeListener?: OnCloseListener; //외부로부터 전달받은 콜백함수를 저장하고 있을 리스너
     constructor(){
         super(`<li class="page-item">
@@ -32,14 +41,14 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
 }
 
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable{
-    constructor(){
+    constructor(private pageItemConstructor: SectionContainerConstructor){
         super('<ul class="page"></ul>');
     }
 //인자로 전달받은 부모 컨테이너에 Page를 추가 HTML에 어떤 것도 받을 수 있고
 //이 함수를 통해서 parent요소에 전달한 값을 등록한다.
     //전달받은 컴포넌트를 PAGEITEMCOMPONENT에 담는다
     addChild(section: Component) {
-        const item = new PageItemComponent();
+        const item = new this.pageItemConstructor(); //외부에서 전달받아서 만듦
         item.addChild(section);
         item.attachTo(this.element, 'beforeend');
         item.setOnCloseListener(() => {
